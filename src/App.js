0e5ffc5985a998dd38cb49ac0af033451431b3f0
codeByCharles <charles.wambua@roam-electric.com>
2024-07-24 16:42:38 +0300

    adds

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './pages/Aside';
 import Register from './pages/Register';
-
+import * as Sentry from '@sentry/react';
 
 function App() {
+  // React.useEffect(() => {
+  //   // Trigger a test error to verify Sentry setup
+  //   Sentry.captureException(new Error("Test Sentry bububububu setup"));
+  // }, []);
+
   return (
-<Router>
-  <Routes>
-   <Route path='/' Component={Sidebar}/>
-   <Route path='/register' Component={Register}/>
-  </Routes>
-</Router>
+    <Router>
+      <Routes>
+        <Route path='/' Component={Sidebar} />
+        <Route path='/register' Component={Register} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,31 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import feather from 'feather-icons';
+import * as Sentry from '@sentry/react';
+
+
+Sentry.init({
+  dsn: "https://3abaa4cc8f2fb123dfa23b0df51c86cf@o4507651680108544.ingest.us.sentry.io/4507655565803520",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Sentry.ErrorBoundary fallback={"An error has occurred"}>
+      <App />
+    </Sentry.ErrorBoundary>
   </React.StrictMode>
 );
+
 feather.replace();
